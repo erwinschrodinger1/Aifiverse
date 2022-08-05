cf459fe4fe4834a689f8cacb1ff3ddc7c010f594
erwinschrodinger1 <81609547+erwinschrodinger1@users.noreply.github.com>
2022-08-05 23:53:36 +0545

    Solved control bugs

--- Assets/Scripts/CharacterControl.cs
+++ Assets/Scripts/CharacterControl.cs
@@ -7,6 +7,10 @@ public class CharacterControl : MonoBehaviour
     GameObject fppCamera;
     Joystick joystick;
     public float speed = 1f;
+    float mouseX = 0;
+    float mouseY = 0;
+    float xRotation;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,9 +21,14 @@ public class CharacterControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        mouseX += Input.GetAxis("Mouse X") * 0.01f;
+        mouseY -= Input.GetAxis("Mouse Y") * 2f;
+        mouseY = Mathf.Clamp(mouseY, -90f, 90f);
+        fppCamera.transform.localRotation = Quaternion.Euler(mouseY, 0f, 0f);
+        transform.Rotate(Vector3.up * mouseX);
         var horizontal = joystick.Horizontal;
         var vertical = joystick.Vertical;
         var moveDirection = transform.forward * vertical + transform.right * horizontal;
-        gameObject.GetComponent<Rigidbody>().AddForce(moveDirection.normalized * speed, ForceMode.Impulse);
+        gameObject.GetComponent<Rigidbody>().AddForce(moveDirection * 0.0001f * speed, ForceMode.Impulse);
     }
 }
