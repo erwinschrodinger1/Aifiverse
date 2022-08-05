8513f13940fdf16c96c2c6a12ebae77bdc47ec75
erwinschrodinger1 <81609547+erwinschrodinger1@users.noreply.github.com>
2022-08-05 22:06:00 +0545

    Added mobile gesture player controls

--- Assets/Scripts/CharacterControl.cs
+++ Assets/Scripts/CharacterControl.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class CharacterControl : MonoBehaviour
 {
+    Camera camera;
     // Start is called before the first frame update
     void Start()
     {
-        
+        camera = GetComponent<Camera>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float pointer_x = Input.GetAxis("Mouse X");
+        float pointer_y = Input.GetAxis("Mouse Y");
+        if (Input.touchCount > 0)
+        {
+            pointer_x = Input.touches[0].deltaPosition.x;
+            pointer_y = Input.touches[0].deltaPosition.y;
+        }
+        transform.localRotation = Quaternion.Euler(pointer_x, 0f, 0f);
+        camera.transform.eulerAngles = (Vector3.up * pointer_y);
     }
 }
